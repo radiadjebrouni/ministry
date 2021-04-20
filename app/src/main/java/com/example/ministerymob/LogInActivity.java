fa9ac7ee88a5e30adf1f986d849b8dde15da418b
faycal <faycel-action@hotmail.com>
2021-04-20 07:24:34 +0200

    implementing authentication with google

--- app/src/main/java/com/example/ministerymob/LogInActivity.java
+++ app/src/main/java/com/example/ministerymob/LogInActivity.java
@@ -1,6 +1,7 @@
 package com.example.ministerymob;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,15 +10,24 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ministerymob.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,8 +38,18 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
     @BindView(R.id.text_field_pswd_cnxn) EditText passwordEditText;
     @BindView(R.id.button_log_in) Button logInButton;
     @BindView(R.id.activity_logIn_forgotten_password) TextView forgottenPassword;
+    @BindView(R.id.google_log_in) ImageButton googleImageButton;
 
+    private final int RC_GOOGLE_SIGN_IN = 1;
+
+    //firebase authentification
     private FirebaseAuth auth;
+
+    //utilisateur firebase
+    private FirebaseUser currentUser;
+
+    //for google signe in
+    GoogleSignInClient googleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +61,17 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
 
         logInButton.setOnClickListener(this);
         forgottenPassword.setOnClickListener(this);
+        googleImageButton.setOnClickListener(this);
         
         //intanciate firebase auth
         auth = FirebaseAuth.getInstance();
+
+        //instanciate google sign in
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this,gso);
     }
 
     @Override
@@ -59,6 +87,10 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
             Intent intent = new Intent(LogInActivity.this,ForgotPasswordActivity.class);
             startActivity(intent);
             finish();
+        }
+        else if(v.getId() == R.id.google_log_in)
+        {
+            googleSignIn();
         }
     }
 
@@ -107,6 +139,53 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
                         else
                         {
                             Toast.makeText(LogInActivity.this, "LogIn failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void googleSignIn()
+    {
+        Intent googleSignInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(googleSignInIntent,RC_GOOGLE_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //retour du resultat du googlesignInIntent
+        if(requestCode == RC_GOOGLE_SIGN_IN)
+        {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Toast.makeText(this, "Utilisateur crée avec succès", Toast.LENGTH_SHORT).show();
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Toast.makeText(this, "Utilisateur non crée", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken)
+    {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            currentUser = auth.getCurrentUser();
+                            Toast.makeText(LogInActivity.this, currentUser.toString(), Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LogInActivity.this, "Utilisateur non crée", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
--- app/src/main/java/com/example/ministerymob/SignUpActivity.java
+++ app/src/main/java/com/example/ministerymob/SignUpActivity.java
@@ -1,6 +1,7 @@
 package com.example.ministerymob;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,30 +10,46 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SignUpActivity extends AppCompatActivity{
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
 
     //les champs de texte (utilisation
     @BindView(R.id.text_field_name_insc) EditText nameEditText;
     @BindView(R.id.text_field_email_insc) EditText emailEditText;
     @BindView(R.id.text_field_pswd_insc) EditText passwordEditText;
     @BindView(R.id.text_field_confirm_pswrd) EditText passwordConfirmationEditText;
+    @BindView(R.id.button_sign_in) Button signInButton;
+    @BindView(R.id.google_sign_in) ImageButton googleImageButton;
 
-    Button signInButton;
+    private final int RC_GOOGLE_SIGN_IN = 1;
 
     //firebase authentification
     private FirebaseAuth auth;
+
+    //utilisateur firebase
+    private FirebaseUser currentUser;
+
+    //for google signe in
+    GoogleSignInClient googleSignInClient;
 
 
     @Override
@@ -46,17 +63,30 @@ public class SignUpActivity extends AppCompatActivity{
         //instanciation de l'object auth
         auth = FirebaseAuth.getInstance();
 
-        signInButton = findViewById(R.id.button_sign_in);
-
-        signInButton.setOnClickListener ( new View.OnClickListener () {
-            @Override
-            public void onClick(View view) {
-                registration();
-            }
-        } );
+        signInButton.setOnClickListener(this);
+        googleImageButton.setOnClickListener(this);
 
         overridePendingTransition(R.anim.slidein_right, R.anim.slide_out_left);
 
+        //instanciate google sign in
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this,gso);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button_sign_in)
+        {
+            registration();
+        }
+        else if(v.getId() == R.id.google_sign_in)
+        {
+            googleSignIn();
+        }
     }
 
     private void registration()
@@ -120,7 +150,7 @@ public class SignUpActivity extends AppCompatActivity{
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()) //si la création est réussite
                         {
-                            FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                            currentUser = FirebaseAuth.getInstance().getCurrentUser();
                             if(currentUser.isEmailVerified()) //if the email is verified
                             {
                                 //upload profile infos
@@ -140,6 +170,51 @@ public class SignUpActivity extends AppCompatActivity{
                         else
                         {
                             Toast.makeText(SignUpActivity.this, "User creation failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void googleSignIn()
+    {
+        Intent googleSignInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(googleSignInIntent,RC_GOOGLE_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //retour du resultat du googlesignInIntent
+        if(requestCode == RC_GOOGLE_SIGN_IN)
+        {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Toast.makeText(this, "Utilisateur crée avec succès", Toast.LENGTH_SHORT).show();
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Toast.makeText(this, "Utilisateur non crée", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            currentUser = auth.getCurrentUser();
+                            Toast.makeText(SignUpActivity.this, currentUser.toString(), Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignUpActivity.this, "Utilisateur non crée", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
